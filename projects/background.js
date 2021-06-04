1caf7dcf5189955bdeab85e37e0ef4b87fee3498
Alexandra Solovyeva <as11303@nyu.edu>
2021-06-04 17:50:18 -0400

    Commit message

--- projects/background.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-function bgChange(bg) {
-	  document.body.style.background = "url(" + bg + ")";
-	  document.body.style.backgroundRepeat = "no-repeat";
-	  document.body.style.backgroundSize = "cover";
-	  document.body.style.backgroundAttachment = "fixed";
-	
-	  
-	}
